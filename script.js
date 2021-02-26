e8ed3bd28771f1ca649d10abfd71f2a013e04599
Melissa Connors <40669268+MelissaConnors@users.noreply.github.com>
2021-02-25 21:22:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
+function rateDisplay()
+{
+	var slider = document.getElementById("rate");
+	var output = document.getElementById("displayRate");
+	output.innerHTML = slider.value.concat("%");
+}
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    	var principal = document.getElementById("principal").value;
+	if (principal <= 0)
+	{
+
+   		alert("The Amount must be greater than zero.");
+
+   		document.getElementById("prinicipal").focus();
+
+   		document.getElementById("prinicipal").click();
+
+   		return null;
+	}
+	
+	var rate = document.getElementById("rate").value;
+	var years = document.getElementById("years").value;
+	
+	var interest = principal * years * rate / 100;
+
+	var currentDate = new Date();
+  	var currentYear = currentDate.getFullYear();
+	var futureYear = parseInt(currentYear) + parseInt(years);
+
+	var finalMessage = "<br>If you deposit " + principal + "," 
+	+ "<br>at an interest rate of " + rate + "%."
+	+ "<br>You will receive an amount of " + interest + ","
+	+ "<br>in the year " + futureYear;  
+	document.getElementById("result").innerHTML = finalMessage;
+
+	document.getElementById("prinicipal").focus();
+
 }
         
